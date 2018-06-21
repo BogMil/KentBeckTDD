1793bb7ea3f740384743b236fa2d3f7da5e52fea
Milan Bogdanovic <milanbogdanovic11@hotmail.com>
2018-06-21 14:44:00 +0200

    Init commit

---
+++ KentBeckTDD/src/Dollar.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KentBeckTDD.src
+{
+    class Dollar
+    {
+        public int amount;
+
+        public Dollar(int amount)
+        {
+            this.amount = amount;
+        }
+
+        public Dollar times(int num)
+        {
+            return new Dollar(amount * num);
+        }
+
+        public bool IsEaqualTo(object Object)
+        {
+            Dollar dollar = (Dollar) Object;
+            return amount == dollar.amount;
+        }
+    }
+}
---
+++ KentBeckTDD/test/MyTest.cs
@@ -0,0 +1,27 @@
+﻿using KentBeckTDD.src;
+using NUnit.Framework;
+
+namespace KentBeckTDD.test
+{
+    [TestFixture]
+
+    class MyTest
+    {
+        [TestCase]
+        public void multiplication()
+        {
+            Dollar five = new Dollar(5);
+            Dollar product = five.times(2);
+            Assert.AreEqual(10, product.amount);
+            product = five.times(3);
+            Assert.AreEqual(15, product.amount);
+        }
+
+        [TestCase]
+        public void testEquality()
+        {
+            Assert.IsTrue(new Dollar(5).IsEaqualTo(new Dollar(5)));
+            Assert.IsFalse(new Dollar(6).IsEaqualTo(new Dollar(5)));
+        }
+    }
+}
